9b2b3419f0fa83acc5fcfbc21f73f0153cbcdd8a
afiolabi <afolabiolagunju@gmail.com>
2026-01-28 00:03:49 +0100

    theming

--- src/components/layout/LandingHeader.tsx
+++ src/components/layout/LandingHeader.tsx
@@ -1,5 +1,19 @@
+import { Switch } from "@/components/ui/switch";
+import { useTheme } from "@/hooks/useTheme";
+import { useEffect, useState } from "react";
+
 const LandingHeader = () => {
-  return <div>LandingHeader</div>;
+  const [changed, SwitchChanged] = useState(false);
+  const { setTheme } = useTheme();
+  useEffect(() => {
+    changed === true ? setTheme("dark") : setTheme("light");
+  }, [changed, setTheme]);
+
+  return (
+    <div>
+      <Switch checked={changed} onCheckedChange={SwitchChanged} />
+    </div>
+  );
 };
 
 export default LandingHeader;
